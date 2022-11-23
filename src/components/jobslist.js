801e7c100588499ce67123d1c2d03d2ba490b2ff
Laura Rodriguez <la.rodriguez@uniandes.edu.co>
2022-11-22 21:06:54 -0500

    retos

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import Job from "./job";
 
-const JobsList = () => {
+import { FormattedMessage } from 'react-intl';
+
+const JobsList = (props) => {
+  const languageColor = props.locale === "es-ES"?"table-light":"table-dark";
   const [offers] = useState([
     {
       id: "0001",
@@ -10,6 +13,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      views: 1,
     },
     {
       id: "0002",
@@ -18,6 +22,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      views: 2,
     },
     {
       id: "0003",
@@ -26,6 +31,7 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      views: 3,
     },
   ]);
 
@@ -33,19 +39,20 @@ const JobsList = () => {
     <div>
       <table className="table">
         <thead className="thead-dark">
-          <tr>
+          <tr className={languageColor}>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col"><FormattedMessage id="Position"/></th>
+            <th scope="col"><FormattedMessage id="Company"/></th>
+            <th scope="col"><FormattedMessage id="Salary"/></th>
+            <th scope="col"><FormattedMessage id="City"/></th>
+            <th scope="col"><FormattedMessage id="PublicationDate"/></th>
+            <th scope="col"><FormattedMessage id="Views"/></th>
           </tr>
         </thead>
         <tbody>
           {console.log("Offers", offers)}
           {offers.map((e, i) => (
-            <Job key={i} offer={e} />
+            <Job key={i} offer={e} locale = {props.locale}/>
           ))}
         </tbody>
       </table>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
 
-import JobsList from "./components/jobslist";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+import getUserLocale from 'get-user-locale';
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import JobsList from "./components/jobsList";
+
+// ReactDOM.render(
+//         <IntlProvider locale="en">
+//                 <JobsList/>
+//         </IntlProvider>, document.getElementById("root")
+// );
+const userLocale = getUserLocale();
+const localeMessages = userLocale==="es-ES"?localeEsMessages:localeEnMessages;
+
+ReactDOM.render(
+        <IntlProvider locale={userLocale} messages={localeMessages} >
+                <JobsList locale={userLocale} />
+        </IntlProvider>, document.getElementById("root")
+);
